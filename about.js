a98b4d68cf53263767a00dd1fd3482360cd75d8b
andrewpokorny <apokorny1@babson.edu>
2020-11-29 20:31:36 -0500

    1

---
+++ about.js
@@ -0,0 +1,24 @@
+const slides = [
+    "images/me.jpg",
+    "images/mayorscup.jpg",
+    "images/akeem.jpg",
+    "images/babsontrack.jpg",
+    "images/bicic.jpg",
+    "images/brooklynboros.jpg",
+    "images/citychamps.jpg",
+    "images/cityxc.jpg",
+    "images/mayorscup2.jpg",
+    "images/stmichaels.jpg",
+];
+let i = 0;
+function slideShow(){
+ let image1 = document.getElementById("slide");
+ 
+ if (i === slides.length){
+     i = 0;
+ }
+ image1.src = slides[i];
+ i++;
+ setTimeout(slideShow, 2000);
+}
+document.querySelector("body").onload = slideShow; 
---
+++ eluidkipchoge.js
@@ -0,0 +1,54 @@
+const PortraitBtn = document.getElementById("Portrait");
+const LondonBtn = document.getElementById("London");
+const OlympicsBtn = document.getElementById("Olympics");
+const INEOSBtn = document.getElementById("INEOS");
+const image = document.getElementById("img1");
+const heading = document.querySelector("h2"); 
+
+function London(){
+   image.src = "images/Kipchogeportrait.jpg";
+   heading.innerText = "London Marathon";
+   heading.style.backgroundColor = "green"
+   image.style.borderColor = "green";
+   LondonBtn.style.backgroundColor = "red";
+   PortraitBtn.style.backgroundColor = "white";
+   INEOSBtn.style.backgroundColor = "white";
+   OlympicsBtn.style.backgroundColor = "white";
+ }
+ LondonBtn.onclick = London;
+
+ function Portrait(){
+image.src = "images/kipchoge.jpg";
+heading.innerText = "Portrait";
+heading.style.backgroundColor = "red"
+image.style.borderColor = "red";
+LondonBtn.style.backgroundColor = "white";
+PortraitBtn.style.backgroundColor = "red";
+INEOSBtn.style.backgroundColor = "white";
+OlympicsBtn.style.backgroundColor = "white";
+ }
+ PortraitBtn.onclick = Portrait;
+
+ function INEOS(){
+image.src = "images/kiprunning.jpg";
+heading.innerText = "INEOS";
+heading.style.backgroundColor = "yellow"
+image.style.borderColor = "yellow";
+LondonBtn.style.backgroundColor = "white";
+PortraitBtn.style.backgroundColor = "white";
+INEOSBtn.style.backgroundColor = "red";
+OlympicsBtn.style.backgroundColor = "white";
+ }
+ INEOSBtn.onclick = INEOS;
+
+ function Olympics(){
+   image.src = "images/olympics.jpg";
+   heading.innerText = " 2016 Olympics";
+   heading.style.backgroundColor = "blue"
+   image.style.borderColor = "blue";
+   LondonBtn.style.backgroundColor = "white";
+   PortraitBtn.style.backgroundColor = "white";
+   INEOSBtn.style.backgroundColor = "white";
+   OlympicsBtn.style.backgroundColor = "red";
+ }
+ OlympicsBtn.onclick = Olympics;
